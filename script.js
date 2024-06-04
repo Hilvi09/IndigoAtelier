e7343d6af15d7170b1c592f65216c77e41d77d99
Hilary Villalobos Pizarro <31682311+Hilvi09@users.noreply.github.com>
2024-06-04 15:00:07 -0600

    Commit 1
    Add files

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const section = document.querySelector(this.getAttribute('href'));
+            section.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
