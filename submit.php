ecbedd6481ff984bc3298c75946ff2f3d05764d0
Hilary Villalobos Pizarro <hilary1809villa@gmail.com>
2024-06-04 15:11:12 -0600

    Commit 2
    Shoppers template

--- submit.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $message = htmlspecialchars($_POST['message']);
-
-    echo "<h1>Thank You, $name!</h1>";
-    echo "<p>Your message: $message</p>";
-}
-?>
